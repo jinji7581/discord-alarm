b40868c8c9c9873783b3e2bcad97472618d5476f
jinji7581 <araara3776@gmail.com>
2025-12-14 16:40:40 +0900

    実験の結果より修正したもの

--- src/bot.py
+++ src/bot.py
@@ -8,6 +8,8 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
+intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
